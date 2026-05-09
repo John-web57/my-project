34dd15ab46db0ea1242df29fdc6bab0c836a338c
John Joshua Omiti <johnomitijoshua@gamil.com>
2026-05-09 22:01:56 +0300

    Initial commit: Data Monks CRM website clone

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('consultation-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    alert(`Thank you, ${name}! We have received your message: "${message}". We will contact you at ${email} soon.`);
+    // In a real backend, this would send to server
+});
